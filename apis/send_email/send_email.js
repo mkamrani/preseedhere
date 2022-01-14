f0b2868b8ad0aff2f02d6eb900802bc40aed94de
Mohsen Kamrani <mohsen.kamrani@austcommunitymedia.com.au>
2022-01-14 17:20:51 +1100

    fix send email

--- apis/send_email/send_email.js
+++ apis/send_email/send_email.js
@@ -5,17 +5,19 @@ exports.handler = async function (event, context) {
   console.log(`email: ${event.body.email}`);
 
   const data = {
-    personalizations: [
-      {
-        to: process.env.RECEIVER,
-        subject: 'preseedhere new sub'
-      }
-    ],
-    from: {
+    personalizations:
+      [
+        {
+          to:
+            [{ email: process.env.RECEIVER }]
+        }]
+    , from: {
       email: process.env.SENDER
     },
-    content: event.body.email
-  };
+    subject: "preseedhere new sub",
+    content: [{ type: "text/plain", value: event.body.email }
+    ]
+  }
 
   const url = "https://api.sendgrid.com/v3/mail/send";
   config = {
@@ -27,41 +29,6 @@ exports.handler = async function (event, context) {
   await axios.post(url, data, config);
   console.log(`sent email`);
 
-  // const options = {
-  //   hostname: "https://api.sendgrid.com",
-  //   port: 443,
-  //   path: '/v3/mail/send',
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     Authorization: `Bearer ${process.env.TOKEN}`,
-  //     'Content-Length': data.length
-  //   }
-  // }
-
-  // console.log(`sending`);
-  // await httpRequest(options);
-  // console.log(`sent`);
-
-  // const req = https.request(options, (res) => {
-  //   let data = '';
-
-  //   res.on('data', (chunk) => {
-  //     data += chunk;
-  //   });
-
-  //   res.on('end', () => {
-  //     console.log(JSON.parse(data));
-  //   });
-
-  // }).on("error", (err) => {
-  //   console.log("Error: ", err.message);
-  // });
-
-  // req.write(data);
-  // req.end();
-  // console.log(`sent the email`);
-
   var response = {
     "statusCode": 200,
     "headers": {
@@ -71,30 +38,3 @@ exports.handler = async function (event, context) {
   }
   return response
 }
-
-// function httpRequest(options) {
-//   return new Promise((resolve, reject) => {
-//     const req = http.request(options, (res) => {
-//       if (res.statusCode < 200 || res.statusCode >= 300) {
-//         return reject(new Error('statusCode=' + res.statusCode));
-//       }
-//       var body = [];
-//       res.on('data', function (chunk) {
-//         body.push(chunk);
-//       });
-//       res.on('end', function () {
-//         try {
-//           body = JSON.parse(Buffer.concat(body).toString());
-//         } catch (e) {
-//           reject(e);
-//         }
-//         resolve(body);
-//       });
-//     });
-//     req.on('error', (e) => {
-//       reject(e.message);
-//     });
-//     // send the request
-//     req.end();
-//   });
-// }
