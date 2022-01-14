dc104269f8d8f914272d62cff3d725c1fee992c0
Mohsen Kamrani <mohsen.kamrani@austcommunitymedia.com.au>
2022-01-14 17:01:55 +1100

    debug

--- apis/send_email/send_email.js
+++ apis/send_email/send_email.js
@@ -1,9 +1,10 @@
-const https = require('https')
+const axios = require('axios')
+const axios = require('axios')
 
 exports.handler = async function (event, context) {
   console.log(`email: ${event.body.email}`);
 
-  const data = JSON.stringify({
+  const data = {
     personalizations: [
       {
         to: process.env.RECEIVER,
@@ -14,23 +15,33 @@ exports.handler = async function (event, context) {
       email: process.env.SENDER
     },
     content: event.body.email
-  })
+  };
 
-  const options = {
-    hostname: "https://api.sendgrid.com",
-    port: 443,
-    path: '/v3/mail/send',
-    method: 'POST',
+  const url = "https://api.sendgrid.com/v3/mail/send";
+  config = {
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${process.env.TOKEN}`,
-      'Content-Length': data.length
     }
   }
+  await axios.post(url, data, config);
+  console.log(`sent email`);
 
-  console.log(`sending`);
-  await httpRequest(options);
-  console.log(`sent`);
+  // const options = {
+  //   hostname: "https://api.sendgrid.com",
+  //   port: 443,
+  //   path: '/v3/mail/send',
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //     Authorization: `Bearer ${process.env.TOKEN}`,
+  //     'Content-Length': data.length
+  //   }
+  // }
+
+  // console.log(`sending`);
+  // await httpRequest(options);
+  // console.log(`sent`);
 
   // const req = https.request(options, (res) => {
   //   let data = '';
@@ -56,35 +67,34 @@ exports.handler = async function (event, context) {
     "headers": {
       "Content-Type": "application/json"
     },
-    "isBase64Encoded": false,
     "body": "New subscription from: " + event.body.email
   }
   return response
 }
 
-function httpRequest(options) {
-  return new Promise((resolve, reject) => {
-     const req = http.request(options, (res) => {
-       if (res.statusCode < 200 || res.statusCode >= 300) {
-             return reject(new Error('statusCode=' + res.statusCode));
-         }
-         var body = [];
-         res.on('data', function(chunk) {
-             body.push(chunk);
-         });
-         res.on('end', function() {
-             try {
-                 body = JSON.parse(Buffer.concat(body).toString());
-             } catch(e) {
-                 reject(e);
-             }
-             resolve(body);
-         });
-     });
-     req.on('error', (e) => {
-       reject(e.message);
-     });
-     // send the request
-    req.end();
- });
-}
+// function httpRequest(options) {
+//   return new Promise((resolve, reject) => {
+//     const req = http.request(options, (res) => {
+//       if (res.statusCode < 200 || res.statusCode >= 300) {
+//         return reject(new Error('statusCode=' + res.statusCode));
+//       }
+//       var body = [];
+//       res.on('data', function (chunk) {
+//         body.push(chunk);
+//       });
+//       res.on('end', function () {
+//         try {
+//           body = JSON.parse(Buffer.concat(body).toString());
+//         } catch (e) {
+//           reject(e);
+//         }
+//         resolve(body);
+//       });
+//     });
+//     req.on('error', (e) => {
+//       reject(e.message);
+//     });
+//     // send the request
+//     req.end();
+//   });
+// }
