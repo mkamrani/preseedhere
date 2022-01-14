599b79e70d712b332b2ac2bdf3c410ff85cec976
Mohsen Kamrani <mohsen.kamrani@austcommunitymedia.com.au>
2022-01-14 17:25:05 +1100

    use trycatch

--- apis/send_email/send_email.js
+++ apis/send_email/send_email.js
@@ -19,14 +19,18 @@ exports.handler = async function (event, context) {
   }
 
   const url = "https://api.sendgrid.com/v3/mail/send";
-  config = {
+  const config = {
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${process.env.TOKEN}`,
     }
   }
-  await axios.post(url, data, config);
-  console.log(`sent email`);
+  try {
+    await axios.post(url, data, config);
+  } catch (error) {
+    console.log(`failed to send the email`);
+  }
+  console.log(`email sent successfully`);
 
   var response = {
     "statusCode": 200,
