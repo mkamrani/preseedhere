1978434f246db57cb5d14177ea0dbb5d8003490c
Mohsen Kamrani <mohsen.kamrani@austcommunitymedia.com.au>
2022-08-03 13:24:22 +1000

    Hide the navbar sign out button on mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -91,7 +91,7 @@ export default function Navbar() {
                 {isLoggedIn && <button
                   onClick={() => logout()}
                   type="button"
-                  className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                  className="hidden lg:block bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                 >
                   Sign out
                 </button>
